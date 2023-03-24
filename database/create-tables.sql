2d019358db9e2e75390db3a1643ae98aa391b3d6
John Wennerberg <johnwennerberg@Johns-MacBook-Pro.local>
2023-03-24 12:00:47 +0100

    Implemented ability to embed google forms, still needs sanitazion

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -6,7 +6,8 @@ CREATE TABLE events (
   eventOrganizer VARCHAR(255),
   eventTicketLimit INT,
   eventDescription VARCHAR(255),
-  eventImage LONGBLOB
+  eventImage LONGBLOB,
+  eventForms VARCHAR(255)
 );
 
 CREATE TABLE accounts (
@@ -23,13 +24,13 @@ CREATE TABLE comments (
   FOREIGN KEY(commentEventID) REFERENCES events(eventID)
 );
 
-INSERT INTO events (eventTitle, eventDate, eventSalesDate, eventOrganizer, eventTicketLimit, eventDescription, eventImage ) VALUES 
-("Festival 1", "2023-01-31", "2023-01-30 16:00:00", "John",  4, "Eddie Meduzas Greatest Hits", ""),
-("Festival 2", "2023-03-31", "2023-01-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", ""),
-("Festival 3", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", ""),
-("Festival 4", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", ""), 
-("Festival 5", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", ""),
-("Festival 6", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "");
+INSERT INTO events (eventTitle, eventDate, eventSalesDate, eventOrganizer, eventTicketLimit, eventDescription, eventImage, eventForms) VALUES 
+("Festival 1", "2023-01-31", "2023-01-30 16:00:00", "John",  4, "Eddie Meduzas Greatest Hits", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>'),
+("Festival 2", "2023-03-31", "2023-01-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>'),
+("Festival 3", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>'),
+("Festival 4", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>'), 
+("Festival 5", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>'),
+("Festival 6", "2023-03-31", "2023-03-30 16:00:00", "John",  4, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta tenetur cum saepe?", "", '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdKEqgyLrUpa6C68T5_7wuxaTpCs1TqQT_dIkcF4XmRjeESTg/viewform?embedded=true" width="640" height="694" frameborder="0" marginheight="0" marginwidth="0">Läser in …</iframe>');
 
 INSERT INTO comments(commentAuthor, commentBody, commentEventID) VALUES
 ("John", "This event was amazing", 4),
